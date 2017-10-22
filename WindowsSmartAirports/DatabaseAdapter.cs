bd2274d3876728051f64e86d3e99302a7a2cb16c
daro-umbe <darijus.gal@gmail.com>
2017-10-22 12:20:55 +0200

    Add files via upload

---
+++ WindowsSmartAirports/DatabaseAdapter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace WindowsSmartAirports
+{
+    public static class DatabaseAdapter
+    {
+        private static geartestEntities entities;
+
+        public static geartestEntities GearboxEntities
+        {
+            get
+            {
+                if (entities == null)
+                    return new geartestEntities();
+                else
+                    return entities;
+            }
+        }
+
+    }
+}
---
+++ WindowsSmartAirports/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsSmartAirports
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int price = 0;
+            int.TryParse(textBox2.Text, out price);
+            if (price != 0)
+            {
+                //DatabaseAdapter.GearboxEntities.Payments.Add(new Payments() { id_pass = 1, shop = textBox1.Text, value = price});
+                //DatabaseAdapter.GearboxEntities.SaveChanges();
+
+                geartestEntities geartestEntities = new geartestEntities();
+                geartestEntities.Payments.SqlQuery("insert into Payments (shop, id_pass, value) values ('Apple', 1,142)", new object[0]);
+                geartestEntities.SaveChanges();
+
+                textBox1.Text = "";
+                textBox2.Text = "";
+            }
+         }
+    }
+}
